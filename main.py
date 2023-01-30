5efa3a4fad829742069f2dd06507eb75e1706fb8
Brian Aracena <brianx0311@gmail.com>
2023-01-29 22:52:21 -0400

    Fix regex

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import re
 
 regex_patterns = [
-    ('keywords', r'/^(if|else|print|exit|ifelse)$/g'),
-    ('single-operators', r'/([-+*\/=()&|;:,<>{}[\]])/g'),
-    ('multiple-operators', r'/(===|!==|[+][+=]|-[-=]|=[=<>]|[<>][=<>]|&&|[|][|])/g'),
-    ('char', r'/[a-zA-Z_]\w*/g'),
-    ('num', r'/\b\d+(\.\d*)?([eE][+-]?\d+)?\b/g'),
-    ('white', r'/\s+/g'),
-    ('string', r"('(\\.|[^'])*'|\"(\\.|[^\"])*\")/g"),
-    ('comment', r'/\/\/.*/g'),
-    ('multiline-comment', r'/\/[*](.|\n)*?[*]\//g')
+    ('keywords', r'^(if|else|print|exit|ifelse)$'),
+    ('single-operators', r'([-+*\\/=()&|;:,<>{}[\\]])'),
+    ('multiple-operators', r'(===|!==|[+][+=]|-[-=]|=[=<>]|[<>][=<>]|&&|[|][|])'),
+    ('char', r'[a-zA-Z_]\w*'),
+    ('num', r'\b\d+(\\.\d*)?([eE][+-]?\d+)?\b'),
+    ('white', r'\s+'),
+    ('string', r"('(\\\\.|[^'])*'|\"(\\\\.|[^\"])*\")"),
+    ('singleline-comment', r'\/\/.*'),
+    ('multiline-comment', r'\/[*](.|\n)*?[*]\/')
 ]
 
-regex_patterns = [(name, re.compile(pattern)) for name, pattern in regex_patterns]
