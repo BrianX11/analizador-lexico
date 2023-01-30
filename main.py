8bd0f571480a9f04968beffdac6039e7564f6555
Brian Aracena <brianx0311@gmail.com>
2023-01-29 22:31:22 -0400

    Add regex compile

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import re
+
 regex_patterns = [
-    ('keywords', '/^(if|else|print|exit|ifelse)$/g'),
-    ('single-operators', '/([+-*/=()&|;:,<>{}[\]])/g'),
-    ('multiple-operators', '/(===|!==|[+][+=]|-[-=]|=[=<>]|[<>][=<>]|&&|[|][|])/g)'),
-    ('char', '/[a-zA-Z_]\w*/g'),
-    ('num', '/\b\d+(\.\d*)?([eE][+-]?\d+)?\b/g'),
-    ('white', '/\s+/g'),
-    ('string',"""/('(\\.|[^'])*'|"(\\.|[^"])*")/g')""")
-    ('comment', '/\/\/.*/g'),
-    ('multiline-comment','/\/[*](.|\n)*?[*]\//g')
+    ('keywords', r'/^(if|else|print|exit|ifelse)$/g'),
+    ('single-operators', r'/([-+*\/=()&|;:,<>{}[\]])/g'),
+    ('multiple-operators', r'/(===|!==|[+][+=]|-[-=]|=[=<>]|[<>][=<>]|&&|[|][|])/g'),
+    ('char', r'/[a-zA-Z_]\w*/g'),
+    ('num', r'/\b\d+(\.\d*)?([eE][+-]?\d+)?\b/g'),
+    ('white', r'/\s+/g'),
+    ('string', r"('(\\.|[^'])*'|\"(\\.|[^\"])*\")/g"),
+    ('comment', r'/\/\/.*/g'),
+    ('multiline-comment', r'/\/[*](.|\n)*?[*]\//g')
 ]
 
-
+regex_patterns = [(name, re.compile(pattern)) for name, pattern in regex_patterns]
